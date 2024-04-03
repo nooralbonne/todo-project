a34468b79fa91cf52e10fd2191f9e35b154da9cc
nooralbonne <noorbonne8@gmail.com>
2024-04-03 02:52:48 +0000

    editting 4-3-2024

--- app.js
+++ app.js
@@ -1,71 +1,42 @@
-var name = prompt("Please enter your name:");
-var gender = prompt("Please enter your gender (male or female):");
-var age = parseInt(prompt("Please enter your age:"));
+"use strict";
+
+//popup box
+//1- alert
+//alert("welcome from code titens Teams")
  
-// Function to prompt the user and collect answers
-function collectAnswers(question) {
-    let answer = prompt(question);
-    // If the answer is empty, consider it as "invalid"
-    if (!answer) {
-        answer = "invalid";
-    }
-    return answer;
+//2- promot
+var Name = prompt("Please Enter Your Name?")
+
+var Gender = prompt("Please Enter Your Gender? (Male/Female)")
+
+var Age = prompt("Please Enter Your Age?")
+if (Age <= 0) {
+    alert("The age should not less than or equal to zero")
 }
 
-// Function to collect multiple answers and store them in an array
-function collectMultipleAnswers(questions) {
-    let answers = [];
-    for (let i = 0; i < questions.length; i++) {
-        let answer = collectAnswers(questions[i]);
-        answers.push(answer);
-    }
-    return answers;
-}
+//3- confirm box
+var  welcomingMessage = confirm("Do you wants to skip the welcoming message?")
 
-// Main function to run the program
-function main() {
-    const questions = [
-        "Are you ready to start?",
-        "Do you like coding?",
-        "Have you ever visited our website?",
-        "Are you satisfied with our service?"
-    ];
-    const answers = collectMultipleAnswers(questions);
-    // Print the answers array to the console
-    console.log("User answers:", answers);
-}
+if (welcomingMessage) {
+    console.log("User chose to skip the welcoming message.");
+} else {
+    displayWelcomeMessage();
+} 
 
-// Call the main function to start the program
-main();
-
-    if (age <= 0) {
-        alert("Invalid age!");
-    } 
-else{
-    if (gender === 'male') {      
-        var skipWelcome = confirm("Do you want to skip the welcoming message?");    
-        if (!skipWelcome)
-        {
-            alert("Welcome, Mr. " + name + "!");
- 
-        }
- 
-    } 
-    else if (gender === 'female') {
-        var skipWelcome = confirm("Do you want to skip the welcoming message?");
-        if (!skipWelcome)
-        {
-            alert("Welcome, Ms. " + name + "!");
-        }
-    }
- 
-    else {
-        var skipWelcome = confirm("Do you want to skip the welcoming message?");
-        if (!skipWelcome)
-        {
-            alert("Welcome, " + name + "!");
-        }
-        
-        
+function displayWelcomeMessage(){
+    if (Gender == "Male" || "male") {
+        alert("Hello, Mr " + Name + "!");
+    } else if (Gender == "Female" || "female") {
+        alert("Hello, Ms " + Name + "!");
+    } else {
+        alert("Hello " + Name + "!");
     }
 }
+
+
+
+
+
+
+
+
